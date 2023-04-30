53d7c5fdcacf41a470ced0d47f6c90a5b6314903
DavidBobek <55330836+DavidBobek@users.noreply.github.com>
2023-04-30 15:31:57 +0200

    s

--- classicedition.js
+++ classicedition.js
@@ -1,46 +1,33 @@
-function Square(
-  name,
-  pricetext,
-  color,
-  price,
-  groupNumber,
-  baserent,
-  rent1,
-  rent2,
-  rent3,
-  rent4,
-  rent5,
-  token
-) {
-  this.name = name;
-  this.pricetext = pricetext;
-  this.color = color;
-  this.owner = 0;
-  this.mortgage = false;
-  this.house = 0;
-  this.hotel = 0;
-  this.token = token;
-  this.groupNumber = groupNumber || 0;
-  this.price = price || 0;
-  this.baserent = baserent || 0;
-  this.rent1 = rent1 || 0;
-  this.rent2 = rent2 || 0;
-  this.rent3 = rent3 || 0;
-  this.rent4 = rent4 || 0;
-  this.rent5 = rent5 || 0;
-  this.landcount = 0;
+function Square(name, pricetext, color, price, groupNumber, baserent, rent1, rent2, rent3, rent4, rent5, token) {
+    this.name = name;
+    this.pricetext = pricetext;
+    this.color = color;
+    this.owner = 0;
+    this.mortgage = false;
+    this.house = 0;
+    this.hotel = 0;
+    this.token = token;
+    this.groupNumber = groupNumber || 0;
+    this.price = price || 0;
+    this.baserent = baserent || 0;
+    this.rent1 = rent1 || 0;
+    this.rent2 = rent2 || 0;
+    this.rent3 = rent3 || 0;
+    this.rent4 = rent4 || 0;
+    this.rent5 = rent5 || 0;
+    this.landcount = 0;
 
-  if (groupNumber === 3 || groupNumber === 4) {
-    this.houseprice = 50;
-  } else if (groupNumber === 5 || groupNumber === 6) {
-    this.houseprice = 100;
-  } else if (groupNumber === 7 || groupNumber === 8) {
-    this.houseprice = 150;
-  } else if (groupNumber === 9 || groupNumber === 10) {
-    this.houseprice = 200;
-  } else {
-    this.houseprice = 0;
-  }
+    if (groupNumber === 3 || groupNumber === 4) {
+        this.houseprice = 50;
+    } else if (groupNumber === 5 || groupNumber === 6) {
+        this.houseprice = 100;
+    } else if (groupNumber === 7 || groupNumber === 8) {
+        this.houseprice = 150;
+    } else if (groupNumber === 9 || groupNumber === 10) {
+        this.houseprice = 200;
+    } else {
+        this.houseprice = 0;
+    }
 }
 
 function Card(text, action) {
@@ -91,347 +78,45 @@ function citytax() {
 var square = [];
 
 square[0] = new Square("GO", "COLLECT $200 SALARY AS YOU PASS.", "#FFFFFF", -1);
-square[1] = new Square(
-  "Paper",
-  "$60",
-  "#8B4513",
-  60,
-  3,
-  2,
-  10,
-  30,
-  90,
-  160,
-  250,
-  2
-);
-square[2] = new Square(
-  "Community Chest",
-  "FOLLOW INSTRUCTIONS ON TOP CARD",
-  "#FFFFFF"
-);
-square[3] = new Square(
-  "Tablet",
-  "$60",
-  "#8B4513",
-  60,
-  3,
-  4,
-  20,
-  60,
-  180,
-  320,
-  450,
-  2
-);
+square[1] = new Square("Paper", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250, 2);
+square[2] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
+square[3] = new Square("Tablet", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450, 2);
 square[4] = new Square("Eco Tax", "Pay $200", "#FFFFFF");
 square[5] = new Square("Bycicle", "$200", "#FFFFFF", 200, 1, 1);
-square[6] = new Square(
-  "Big Ben",
-  "$100",
-  "#87CEEB",
-  100,
-  4,
-  6,
-  30,
-  90,
-  270,
-  400,
-  550,
-  2
-);
+square[6] = new Square("Big Ben", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550, 2);
 square[7] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[8] = new Square(
-  "Watch",
-  "$100",
-  "#87CEEB",
-  100,
-  4,
-  6,
-  30,
-  90,
-  270,
-  400,
-  550,
-  2
-);
-square[9] = new Square(
-  "Smart Watch",
-  "$120",
-  "#87CEEB",
-  120,
-  4,
-  8,
-  40,
-  100,
-  300,
-  450,
-  600,
-  2
-);
+square[8] = new Square("Watch", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550, 2);
+square[9] = new Square("Smart Watch", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600, 2);
 square[10] = new Square("Just Visiting", "", "#FFFFFF");
-square[11] = new Square(
-  "Walkmen",
-  "$140",
-  "#FF0080",
-  140,
-  5,
-  10,
-  50,
-  150,
-  450,
-  625,
-  750,
-  4
-);
+square[11] = new Square("Walkmen", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750, 4);
 square[12] = new Square("Fusion Reactor", "$150", "#FFFFFF", 150, 2);
-square[13] = new Square(
-  "Mp3",
-  "$140",
-  "#FF0080",
-  140,
-  5,
-  10,
-  50,
-  150,
-  450,
-  625,
-  750,
-  4
-);
-square[14] = new Square(
-  "Spotify",
-  "$160",
-  "#FF0080",
-  160,
-  5,
-  12,
-  60,
-  180,
-  500,
-  700,
-  900,
-  4
-);
+square[13] = new Square("Mp3", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750, 4);
+square[14] = new Square("Spotify", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900, 4);
 square[15] = new Square("Train", "$200", "#FFFFFF", 200, 1, 2);
-square[16] = new Square(
-  "Foto Album",
-  "$180",
-  "#FFA500",
-  180,
-  6,
-  14,
-  70,
-  200,
-  550,
-  750,
-  950,
-  4
-);
-square[17] = new Square(
-  "Community Chest",
-  "FOLLOW INSTRUCTIONS ON TOP CARD",
-  "#FFFFFF"
-);
-square[18] = new Square(
-  "Digital Album",
-  "$180",
-  "#FFA500",
-  180,
-  6,
-  14,
-  70,
-  200,
-  550,
-  750,
-  950,
-  4
-);
-square[19] = new Square(
-  "Cloud Album",
-  "$200",
-  "#FFA500",
-  200,
-  6,
-  16,
-  80,
-  220,
-  600,
-  800,
-  1000,
-  4
-);
+square[16] = new Square("Foto Album", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950, 4);
+square[17] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
+square[18] = new Square("Digital Album", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950, 4);
+square[19] = new Square("Cloud Album", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000, 4);
 square[20] = new Square("Free Parking", "", "#FFFFFF");
-square[21] = new Square(
-  "Landline Phone",
-  "$220",
-  "#FF0000",
-  220,
-  7,
-  18,
-  90,
-  250,
-  700,
-  875,
-  1050,
-  6
-);
+square[21] = new Square("Landline Phone", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050, 6);
 square[22] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[23] = new Square(
-  "Mobile Phone",
-  "$220",
-  "#FF0000",
-  220,
-  7,
-  18,
-  90,
-  250,
-  700,
-  875,
-  1050,
-  6
-);
-square[24] = new Square(
-  "Social Media",
-  "$240",
-  "#FF0000",
-  240,
-  7,
-  20,
-  100,
-  300,
-  750,
-  925,
-  1100,
-  6
-);
+square[23] = new Square("Mobile Phone", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050, 6);
+square[24] = new Square("Social Media", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100, 6);
 square[25] = new Square("Car", "$200", "#FFFFFF", 200, 1, 3);
-square[26] = new Square(
-  "Theatre",
-  "$260",
-  "#FFFF00",
-  260,
-  8,
-  22,
-  110,
-  330,
-  800,
-  975,
-  1150,
-  6
-);
-square[27] = new Square(
-  "Cinema",
-  "$260",
-  "#FFFF00",
-  260,
-  8,
-  22,
-  110,
-  330,
-  800,
-  975,
-  1150,
-  6
-);
+square[26] = new Square("Theatre", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150, 6);
+square[27] = new Square("Cinema", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150, 6);
 square[28] = new Square("Data Processing Center", "$150", "#FFFFFF", 150, 2);
-square[29] = new Square(
-  "Netflix",
-  "$280",
-  "#FFFF00",
-  280,
-  8,
-  24,
-  120,
-  360,
-  850,
-  1025,
-  1200,
-  6
-);
-square[30] = new Square(
-  "Go to Jail",
-  "Go directly to Jail. Do not pass GO. Do not collect $200.",
-  "#FFFFFF"
-);
-square[31] = new Square(
-  "Library",
-  "$300",
-  "#008000",
-  300,
-  9,
-  26,
-  130,
-  390,
-  900,
-  1100,
-  1275,
-  8
-);
-square[32] = new Square(
-  "Google",
-  "$300",
-  "#008000",
-  300,
-  9,
-  26,
-  130,
-  390,
-  900,
-  1100,
-  1275,
-  8
-);
-square[33] = new Square(
-  "Community Chest",
-  "FOLLOW INSTRUCTIONS ON TOP CARD",
-  "#FFFFFF"
-);
-square[34] = new Square(
-  "Chat GPT",
-  "$320",
-  "#008000",
-  320,
-  9,
-  28,
-  150,
-  450,
-  1000,
-  1200,
-  1400,
-  8
-);
+square[29] = new Square("Netflix", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200, 6);
+square[30] = new Square("Go to Jail", "Go directly to Jail. Do not pass GO. Do not collect $200.", "#FFFFFF");
+square[31] = new Square("Library", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275, 8);
+square[32] = new Square("Google", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275, 8);
+square[33] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
+square[34] = new Square("Chat GPT", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400, 8);
 square[35] = new Square("Plane", "$200", "#FFFFFF", 200, 1, 4);
 square[36] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[37] = new Square(
-  "Online Banking",
-  "$350",
-  "#0000FF",
-  350,
-  10,
-  35,
-  175,
-  500,
-  1100,
-  1300,
-  1500,
-  8
-);
+square[37] = new Square("Online Banking", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500, 8);
 square[38] = new Square("LUXURY TAX", "Pay $100", "#FFFFFF");
-square[39] = new Square(
-  "Crypto",
-  "$400",
-  "#0000FF",
-  400,
-  10,
-  50,
-  200,
-  600,
-  1400,
-  1700,
-  2000,
-  8
-);
+square[39] = new Square("Crypto", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000, 8);
 
 var communityChestCards = [];
 var chanceCards = [];
